6ae8a0466697f8adb3364a4f0f97ce3d5c1aeb70
Matthew Shafer (laptop) <mshafer1@harding.edu>
2018-11-23 16:08:33 -0600

    got code preview working on desktop

---
+++ js/expander.js
@@ -0,0 +1,28 @@
+
+function expand(id, scrollId) {
+    $('#' + id).removeClass('code-preview');
+    $('#Btn' + id).html("Click to Collapse . . .").off("click");
+    $('#Btn' + id).click(function () {
+        collapse(id, scrollId);
+    })
+    console.log("ScrollID: " + scrollId);
+    scrollToId(scrollId);
+    console.log("expanding . . .");
+}
+
+function collapse(id, scrollId) {
+    $('#' + id).addClass('code-preview')
+    $('#Btn' + id).html("Click to Expand . . .").off("click");
+    $('#Btn' + id).click(function () {
+        expand(id, scrollId);
+    })
+    console.log("ScrollID: " + scrollId);
+    scrollToId(scrollId);
+    console.log("collapsing . . .");
+}
+
+function scrollToId(id) {
+    $('html, body').animate({
+        scrollTop: $('#' + id).offset().top - 50,
+      }, 1000)
+}
